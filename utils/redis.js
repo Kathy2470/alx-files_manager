138bc204752af118bbc60ac64b133b027217fda6
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-09-05 06:57:16 +0000

    Update redis.js

--- utils/redis.js
+++ utils/redis.js
@@ -1,46 +1,12 @@
-// import redis from 'redis';
-
-// const redisClient = redis.createClient({
-//   url: 'redis://localhost:6379', // Adjust as needed
-// });
-
-// redisClient.on('error', (err) => {
-//   console.error('Redis error:', err);
-// });
-
-// export default redisClient;
-
-
-
-
-// import redis from 'redis';
-// import { promisify } from 'util';
-
-// class RedisClient {
-//   constructor() {
-//     this.client = redis.createClient();
-//     this.client.on('error', (err) => console.error('Redis client not connected to the server:', err));
-//   }
-
-//   isAlive() {
-//     return this.client.connected;
-//   }
-
-//   // Add other methods as needed, e.g., for getting or setting values
-// }
-
-// const redisClient = new RedisClient();
-// export default redisClient;
-
-
-
 import redis from 'redis';
 import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.client.on('error', (err) => console.error('Redis client not connected to the server:', err));
+    this.client.on('error', (err) =>
+      console.error('Redis client not connected to the server:', err)
+    );
 
     // Promisify the Redis client methods for easier async/await usage
     this.getAsync = promisify(this.client.get).bind(this.client);
@@ -64,7 +30,7 @@ class RedisClient {
 
   async set(key, value, duration) {
     try {
-      await this.setAsync(key, value, 'EX', duration); // 'EX' sets the expiration in seconds
+      await this.setAsync(key, value, 'EX', duration); // 'EX' sets expiration in seconds
     } catch (err) {
       console.error('Error setting value in Redis:', err);
     }
