41f19b3ee2a9d2c1ce569609bbba0860cb07fb94
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-09-05 07:46:22 +0000

    Update AppController.js

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,6 +1,5 @@
-import redisClient from '../utils/redis.js';
-
-import dbClient from '../utils/db.js';
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
 class AppController {
   static getStatus(req, res) {
@@ -12,11 +11,16 @@ class AppController {
   }
 
   static async getStats(req, res) {
-    const stats = {
-      users: await dbClient.nbUsers(),
-      files: await dbClient.nbFiles(),
-    };
-    res.status(200).send(stats);
+    try {
+      const stats = {
+        users: await dbClient.nbUsers(),
+        files: await dbClient.nbFiles(),
+      };
+      res.status(200).send(stats);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ message: 'Error fetching stats' });
+    }
   }
 }
 
