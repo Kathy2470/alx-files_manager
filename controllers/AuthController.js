3d40da615c2757e5e83f99b8a5b8714590ecf499
markdrahg <neutral520@gmail.com>
2024-09-05 14:41:25 +0000

    Try Test 1

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,79 @@
+// import { v4 as uuidv4 } from 'uuid';
+// import sha1 from 'sha1';
+// import redisClient from '../utils/redis.js';
+// import dbClient from '../utils/db.js';
+
+// class AuthController {
+//   static async getConnect(req, res) {
+//     // Extract the Authorization header
+//     const Authorization = req.header('Authorization') || '';
+
+//     // Split the header to get the credentials part
+//     const credentials = Authorization.split(' ')[1];
+
+//     if (!credentials) {
+//       return res.status(401).send({ error: 'Unauthorized' });
+//     }
+
+//     // Decode the Base64 credentials to get email and password
+//     const decodedCredentials = Buffer.from(credentials, 'base64').toString('utf-8');
+//     const [email, password] = decodedCredentials.split(':');
+
+//     if (!email || !password) {
+//       return res.status(401).send({ error: 'Unauthorized' });
+//     }
+
+//     // Hash the password with SHA1
+//     const sha1Password = sha1(password);
+
+//     // Fetch the user from the database
+//     const user = await dbClient.db.collection('users').findOne({ email, password: sha1Password });
+
+//     if (!user) {
+//       return res.status(401).send({ error: 'Unauthorized' });
+//     }
+
+//     // Generate a new token and create a Redis key
+//     const token = uuidv4();
+//     const key = `auth_${token}`;
+//     const hoursForExpiration = 24;
+
+//     // Store the user ID in Redis with the generated key, set to expire in 24 hours
+//     await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
+
+//     // Respond with the token
+//     return res.status(200).send({ token });
+//   }
+
+//   static async getDisconnect(req, res) {
+//     // Extract token from headers
+//     const token = req.header('X-Token');
+
+//     if (!token) {
+//       return res.status(401).send({ error: 'Unauthorized' });
+//     }
+
+//     const key = `auth_${token}`;
+//     const userId = await redisClient.get(key);
+
+//     if (!userId) {
+//       return res.status(401).send({ error: 'Unauthorized' });
+//     }
+
+//     // Delete the token from Redis
+//     await redisClient.del(key);
+
+//     // Respond with no content
+//     return res.status(204).send();
+//   }
+// }
+
+// export default AuthController;
+
+
+
+
+
 import { v4 as uuidv4 } from 'uuid';
 import sha1 from 'sha1';
 import redisClient from '../utils/redis.js';
@@ -5,17 +81,13 @@ import dbClient from '../utils/db.js';
 
 class AuthController {
   static async getConnect(req, res) {
-    // Extract the Authorization header
     const Authorization = req.header('Authorization') || '';
-
-    // Split the header to get the credentials part
     const credentials = Authorization.split(' ')[1];
 
     if (!credentials) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
 
-    // Decode the Base64 credentials to get email and password
     const decodedCredentials = Buffer.from(credentials, 'base64').toString('utf-8');
     const [email, password] = decodedCredentials.split(':');
 
@@ -23,30 +95,24 @@ class AuthController {
       return res.status(401).send({ error: 'Unauthorized' });
     }
 
-    // Hash the password with SHA1
     const sha1Password = sha1(password);
 
-    // Fetch the user from the database
     const user = await dbClient.db.collection('users').findOne({ email, password: sha1Password });
 
     if (!user) {
       return res.status(401).send({ error: 'Unauthorized' });
     }
 
-    // Generate a new token and create a Redis key
     const token = uuidv4();
     const key = `auth_${token}`;
     const hoursForExpiration = 24;
 
-    // Store the user ID in Redis with the generated key, set to expire in 24 hours
     await redisClient.set(key, user._id.toString(), hoursForExpiration * 3600);
 
-    // Respond with the token
     return res.status(200).send({ token });
   }
 
   static async getDisconnect(req, res) {
-    // Extract token from headers
     const token = req.header('X-Token');
 
     if (!token) {
@@ -60,10 +126,8 @@ class AuthController {
       return res.status(401).send({ error: 'Unauthorized' });
     }
 
-    // Delete the token from Redis
     await redisClient.del(key);
 
-    // Respond with no content
     return res.status(204).send();
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,54 @@
+// import express from 'express';
+
+// import AppController from '../controllers/AppController.js';
+
+// import UsersController from '../controllers/UsersController.js';
+
+// import AuthController from '../controllers/AuthController.js';
+
+// import FilesController from '../controllers/FilesController.js';
+
+// // Rest of the code
+// function controllerRouting(app) {
+//   const router = express.Router();
+//   app.use('/', router);
+
+//   // App Controller Routes
+//   router.get('/status', AppController.getStatus);
+//   router.get('/stats', AppController.getStats);
+
+//   // User Controller Routes
+//   router.post('/users', UsersController.postNew);
+//   router.get('/users/me', UsersController.getMe);
+
+//   // Auth Controller Routes
+//   router.get('/connect', AuthController.getConnect);
+//   router.get('/disconnect', AuthController.getDisconnect);
+
+//   // Files Controller Routes
+//   router.post('/files', FilesController.postUpload);
+//   router.get('/files/:id', FilesController.getShow);
+//   router.get('/files', FilesController.getIndex);
+//   router.put('/files/:id/publish', FilesController.putPublish);
+//   router.put('/files/:id/unpublish', FilesController.putUnpublish);
+//   router.get('/files/:id/data', FilesController.getFile);
+
+//   router.post('/files', FilesController.postUpload);
+
+// }
+
+// export default controllerRouting;
+
+
+
+
+
 import express from 'express';
+import AppController from '../controllers/AppController.js';
+import UsersController from '../controllers/UsersController.js';
+import AuthController from '../controllers/AuthController.js';
+import FilesController from '../controllers/FilesController.js';
 
-import AppController from '../controllers/AppController';
-
-import UsersController from '../controllers/UsersController';
-
-import AuthController from '../controllers/AuthController';
-
-import FilesController from '../controllers/FilesController';
-
-// Rest of the code
 function controllerRouting(app) {
   const router = express.Router();
   app.use('/', router);
@@ -32,9 +72,6 @@ function controllerRouting(app) {
   router.put('/files/:id/publish', FilesController.putPublish);
   router.put('/files/:id/unpublish', FilesController.putUnpublish);
   router.get('/files/:id/data', FilesController.getFile);
-
-  router.post('/files', FilesController.postUpload);
-
 }
 
 export default controllerRouting;
