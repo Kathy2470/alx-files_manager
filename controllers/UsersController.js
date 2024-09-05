2b3a7e7ebf67250ef1e57c3b25254d1befe6dd47
Mark Drah <132355775+markdrahg@users.noreply.github.com>
2024-09-05 08:19:58 +0000

    Update UsersController.js

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,6 @@
-import { ObjectId } from 'mongodb';
+import pkg from 'mongodb';
+const { ObjectId } = pkg;
+import sha1 from 'sha1';
 import Queue from 'bull';
 import dbClient from '../utils/db';
 import userUtils from '../utils/user'; // Import your utility file for getting user by ID
